1a424ee5d774086a7b6e778a68778110e2b1a5ca
Raul <raulaguilar360@gmail.com>
2020-12-14 20:09:43 -0600

    added Procfile

--- index.py
+++ index.py
@@ -1,4 +1,14 @@
-from routing import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import  LoginManager
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ojiukg89j1po1l1h2v36aj2h41oq235qk3hg'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)  
+
+login_manager = LoginManager(app)
+ 
+from routing import routes
